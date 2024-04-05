22e2c964495ea3b7b5d252dbcc63f19abb4b0ba4
NOTIF-API <112265437+NOTIF-API@users.noreply.github.com>
2024-04-05 21:41:33 +0300

    rebuild

---
+++ SCP-600V/Commands/Parent.cs
@@ -0,0 +1,37 @@
+﻿using CommandSystem;
+using Exiled.API.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCP_600V.Commands
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class Parent : ParentCommand
+    {
+        public Parent() => LoadGeneratedCommands();
+
+        public override string Command { get; } = "scp600";
+
+        public override string[] Aliases { get; } = { "sp6" };
+
+        public override string Description { get; } = "Handle behaviour of a plugin scp600v";
+
+        public override void LoadGeneratedCommands()
+        {
+            Log.Debug("Registering other commands");
+            RegisterCommand(new Lists());
+            RegisterCommand(new Spawn());
+            RegisterCommand(new ImetateKill());
+            
+        }
+
+        protected override bool ExecuteParent(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "sp6 slist | spawn";
+            return true;
+        }
+    }
+}
---
+++ SCP-600V/Main.cs
@@ -0,0 +1,64 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.CustomRoles.API;
+using HarmonyLib;
+using System;
+
+namespace SCP_600V
+{
+    internal class Main: Plugin<Config>
+    {
+        public override string Author { get; } = "notifapi";
+
+        public override string Name { get; } = "scp600v";
+
+        public override bool IgnoreRequiredVersionCheck { get; } = false;
+
+        public override Version RequiredExiledVersion { get; } = new Version(8, 6, 0);
+
+        public override Version Version { get; } = new Version(3, 0, 0);
+
+        public override PluginPriority Priority { get; } = PluginPriority.Medium;
+
+        /// <summary>
+        /// A static variable called Instance is created to gain access to the plugin configurations and other parameters
+        /// </summary>
+        public static Main Instance { get; private set; }
+
+        private Harmony Harmony;
+
+        public override void OnEnabled()
+        {
+            Log.Debug("Creating instance of main class");
+            Instance = this;
+            Log.Debug("Creating harmony");
+            Harmony = new Harmony("com.scp600.object");
+            Log.Debug("Patching all in harmony");
+            Harmony.PatchAll();
+            Log.Debug("Registaring role scp600");
+            this.Config.ScpConfig.Register();
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Log.Debug("Unregister role scp600");
+            this.Config.ScpConfig.Unregister();
+            Log.Debug("Unpatching harmony");
+            Harmony.UnpatchAll();
+            Instance = null;
+            Harmony = null;
+            base.OnDisabled();
+        }
+
+        private void RegisterEvent()
+        {
+            Log.Debug("Subscride to events");
+        }
+
+        private void UnRegisterEvent()
+        {
+            Log.Debug("Unsubscride to events");
+        }
+    }
+}
